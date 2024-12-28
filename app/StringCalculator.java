c086427984aa91b05cae78d1b98a5df1a92ce64b
Nayak-Sahil <nayaksahil992003@gmail.com>
2024-12-28 18:16:22 +0530

    [Implement]: Single  Digit Input Case

--- app/StringCalculator.java
+++ app/StringCalculator.java
@@ -5,5 +5,12 @@ public class StringCalculator {
         if(numbers.isEmpty()){
             return 0;
         }
+        
+        int sum = 0;
+        if(numbers.length() == 1){
+            sum = Integer.parseInt(numbers);
+        }
+
+        return sum;
     }
 }
