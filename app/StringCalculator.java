3b909cd025cdf63b4b0da837ab877645580f70f5
Nayak-Sahil <nayaksahil992003@gmail.com>
2024-12-28 19:34:43 +0530

    [Implement]: Logic of custom delimiter

--- app/StringCalculator.java
+++ app/StringCalculator.java
@@ -7,9 +7,23 @@ public class StringCalculator {
         }
 
         int sum = 0;
+        
+        String delimiter = ",";
 
-        // Replace new line characters with commas
-        numbers = numbers.replace("\n", ",");
+        // Check if the input string starts with "//"
+        if (numbers.startsWith("//")) {
+            // Extract the delimiter
+            delimiter = numbers.substring(2, 3);
+
+            // reset the input string to exclude the (delimiter + "\n")
+            numbers = numbers.substring(4);
+        }
+
+        // Replace new line characters with specified delimiter
+        numbers = numbers.replace("\n", delimiter);
+
+        // Replace the delimiter with a comma because if we split by comma in next step then only, we can extract the numbers otherwise delimiter is also included in array.
+        numbers = numbers.replace(delimiter, ",");
 
         // Split the input string by commas to Extract individual numbers
         String[] digits = numbers.split(",");
